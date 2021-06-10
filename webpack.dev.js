f857fc7cde93450fcd6f6e69268ba9d0a99c1bac
MuslumYilmaz <muslum@vizyon.ai>
2021-06-10 13:37:18 +0300

    remove service worker plugin form dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -48,7 +47,6 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        new WorkboxPlugin.GenerateSW()
     ],
     devServer:{
         port: 8081,
