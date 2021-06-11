25a55d0adc93de42eee651bed599e5638c5e2d72
MuslumYilmaz <muslum@vizyon.ai>
2021-06-11 11:27:46 +0300

    css to scss

--- webpack.prod.js
+++ webpack.prod.js
@@ -19,7 +19,7 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
